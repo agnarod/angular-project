c0dda9346b1569b282c2c20aee54bf7771d9471b
agnarod <agustin.nmf@gmail.com>
2023-01-24 14:36:49 -0800

    basic topics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+
+  recipes: RecipeModel[] = [
+    new RecipeModel("A test recipe", "this is simply a test", "https://freesvg.org/img/RecipeBook.png"),
+    new RecipeModel("A test recipe2", "this is simply a test2", "https://freesvg.org/img/RecipeBook.png"),
+    new RecipeModel("A test recipe3", "this is simply a test3", "https://freesvg.org/img/RecipeBook.png"),
+    new RecipeModel("A test recipe4", "this is simply a test4", "https://freesvg.org/img/RecipeBook.png")
+  ];
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { IngredientModel } from "../shared/ingredient.model";
+
+@Component({
+    selector:   'app-shopping-list',
+    templateUrl:'./shopping-list.component.html',
+    styleUrls:['./shopping-list.component.css']
+})
+
+export class ShoppingListComponent{
+
+    ingredients:IngredientModel[] = [
+        new IngredientModel("tomatoes", 5),
+        new IngredientModel("potatoes", 3),
+        new IngredientModel("chillies", 2)
+    ];
+
+    constructor(){
+
+    }
+    ngOnInit(){
+
+    }
+
+}
